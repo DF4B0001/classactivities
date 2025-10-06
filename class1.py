8d41639809f13d0af14b5f926cd17e41305b27a3
DF4B0001 <BeanZf@outlook.com>
2025-10-06 19:57:51 +0100

    V1.0.0

--- class1.py
+++ class1.py
@@ -2,3 +2,7 @@ import pandas as pd
 import matplotlib
 import numpy as np
 c1=pd.read_csv("class1.csv")
+#this code is taken from github.
+# link to the code: https://github.com/DF4B0001/classactivities/blob/master/class1.py
+
+for data in c1:
