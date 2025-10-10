625c5afbaff6644bc95a6885ee97e2a4156a808b
DF4B0001 <BeanZf@outlook.com>
2025-10-10 17:01:00 +0100

    V1.2

--- class1.py
+++ class1.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import matplotlib
 import numpy as np
+from pandas.core.interchange.dataframe_protocol import DataFrame
+
 data=pd.read_csv("npc_codes.csv")
-# print(data)
-for x in data:
-    if x='NaN':
+# delete all rows and columns with NaN
+data.dropna(axis=0,how='all',inplace=True)
+data.dropna(axis=1,how='all',inplace=True)
+data.drop(data.columns[-1],axis=1,inplace=True)\
+data.drop(data.columns[0],axis=1,inplace=True)
+print(data)
