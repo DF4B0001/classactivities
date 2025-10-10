0c8494ea8063453bacd07e5de6ce21e922992693
DF4B0001 <BeanZf@outlook.com>
2025-10-10 17:11:45 +0100

    finalized 20251010

--- class1.py
+++ class1.py
@@ -2,11 +2,11 @@ import pandas as pd
 import matplotlib
 import numpy as np
 from pandas.core.interchange.dataframe_protocol import DataFrame
-
 data=pd.read_csv("npc_codes.csv")
 # delete all rows and columns with NaN
 data.dropna(axis=0,how='all',inplace=True)
-data.dropna(axis=1,how='all',inplace=True)
-data.drop(data.columns[-1],axis=1,inplace=True)\
-data.drop(data.columns[0],axis=1,inplace=True)
-print(data)
+data.dropna(axis=1,how='all',inplace=True) #remove all NaNs from rows and columns
+data.drop(data.columns[-1],axis=1,inplace=True)
+data.drop(data.columns[0],axis=1,inplace=True) #remove NaN in the last row
+data = data.dropna(how='any') #remove the NaN in the bottom right hand corner
+print(data) #cleaning data
