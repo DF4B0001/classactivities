23e0ecd8416474e9a489b710e540c439114732e0
DF4B0001 <BeanZf@outlook.com>
2025-10-11 18:59:40 +0100

    updated 20251011: added 2 prints to check difference before the drop commands are applied.

--- class1.py
+++ class1.py
@@ -3,6 +3,7 @@ import matplotlib as plt
 import numpy as np
 from pandas.core.interchange.dataframe_protocol import DataFrame
 data=pd.read_csv("npc_codes.csv")
+print(data)
 # delete all rows and columns with NaN
 data.dropna(axis=0,how='all',inplace=True)
 data.dropna(axis=1,how='all',inplace=True)
