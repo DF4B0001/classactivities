2aaae3b3e0f2ab9d3d5e00291ee5ce68441ad255
DF4B0001 <BeanZf@outlook.com>
2025-10-11 16:20:53 +0100

    updated 20251011

--- class1.py
+++ class1.py
@@ -7,6 +7,6 @@ data=pd.read_csv("npc_codes.csv")
 data.dropna(axis=0,how='all',inplace=True)
 data.dropna(axis=1,how='all',inplace=True)
 data.drop(data.columns[-1],axis=1,inplace=True)
-data.drop(data.columns[0],axis=1,inplace=True) #remove NaN in the last row
+data.drop(data.columns[0],axis=1,inplace=True) #remove NaN in the last column
 data = data.dropna(how='any') #remove the NaN in the bottom right hand corner
 print(data) #cleaning data
